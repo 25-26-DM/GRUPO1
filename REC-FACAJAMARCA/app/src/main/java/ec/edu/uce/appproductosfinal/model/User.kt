192140c596b5d81c049cc9d53648d6e329d9cfa7
KaiOxS <torito9926@hotmail.es>
2026-02-25 16:20:09 -0500

    Supletorio

--- REC-FACAJAMARCA/app/src/main/java/ec/edu/uce/appproductosfinal/model/User.kt
+++ REC-FACAJAMARCA/app/src/main/java/ec/edu/uce/appproductosfinal/model/User.kt
@@ -5,9 +5,10 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "users")
 data class User(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val nombre: String,
-    val apellido: String,
-    val password: String, // Hash SHA-256
+    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
+    val nombre: String = "",
+    val apellido: String = "",
+    val correo: String = "",
+    val password: String = "", // Hash SHA-256
     val lastUpdated: Long = System.currentTimeMillis()
 )
